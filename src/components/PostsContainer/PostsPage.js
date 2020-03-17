98b11bcd79ab4083e307a24fa5017b0881efdab8
Tommy <tommykindle83@gmail.com>
2020-03-17 09:52:24 -0700

    setting up posts and likes

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,18 +1,20 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
-// import data 
+import dummyData from "../../dummy-data";
+
 
 const PostsPage = () => {
-  // set up state for your data
+  const [postData, setPostData] = useState(dummyData);
   return (
     <div className="posts-container-wrapper">
-      {/* map through data here to return a Post and pass data as props to Post */}
+      {postData.map(data => (
+        <Post key={data.username} post={data} />
+      ))}
     </div>
   );
 };
 
 export default PostsPage;
-
